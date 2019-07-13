7a1a997d8f0e47906e3c9359aa7583d6f1a2d363
planet <alhussain@iut-dhaka.edu>
2019-07-13 20:18:51 +0600

    main activity update

--- app/src/main/java/com/example/health_worker/MainActivity.java
+++ app/src/main/java/com/example/health_worker/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.health_worker;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
